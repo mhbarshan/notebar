80b94ae5c1d432f4d1fe5f2421680afcd375f093
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2024-04-10 01:07:45 +0600

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://notebar-be.onrender.com";
   const notesInitial = [];
   //Get All Notes
   const getNote = async () => {
