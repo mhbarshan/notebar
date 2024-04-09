a9dd784f8c6d489bd814022ee2bf1416419d39b8
mhbarshan <64357462+mhbarshan@users.noreply.github.com>
2024-04-10 01:43:36 +0600

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const host = "http://localhost:5000";
+const host = "https://notebar-be.onrender.com";
 
 function Login(props) {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
